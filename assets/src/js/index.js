4cfe196b1d8813f5b13514b25242a7f00f1785ec
Fabien Mottier <fabien.mottier@he-arc.ch>
2019-02-25 14:11:38 +0100

    prepare filter

--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -1,1 +1,12 @@
 import '../scss/index.scss';
+import '../../../node_modules/@fortawesome/fontawesome-free/js/all.min.js';
+
+$(document).ready(function() {
+    $("#filter-genre").click(function() {
+        $(".dropdown").toggleClass("is-active");
+    });
+
+    $("#filter-genre").blur(function() {
+        $(".dropdown").removeClass("is-active");
+    });
+});
