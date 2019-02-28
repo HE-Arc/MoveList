eee06685a5ba73cc28253ca6cd007fff7dfaef42
bull0n <lucas.bulloni@protonmail.ch>
2019-02-28 22:05:14 +0100

    sign up and login with bulm

--- accounts/views.py
+++ accounts/views.py
@@ -6,5 +6,8 @@ from django.views import generic
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
+
+    print(form_class)
+
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
