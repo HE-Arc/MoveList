e48b1be99d2d6e040d136a5cb718580d3854510f
bull0n <lucas.bulloni@protonmail.ch>
2019-03-01 22:18:56 +0100

    change react file name and make a solution to render a component for each page

--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -1,20 +1,26 @@
 import '../scss/index.scss';
 import '../../../node_modules/@fortawesome/fontawesome-free/js/all.min.js';
-import Main from './main.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-/*$(document).ready(function() {
-    $("#filter-genre").click(function() {
-        $(".dropdown").toggleClass("is-active");
-    });
+import MainComponent from './MainComponent.js';
 
-    $("#filter-genre").blur(function() {
-        $(".dropdown").removeClass("is-active");
-    });
-});*/
+// https://stackoverflow.com/questions/31933359/using-react-in-a-multi-page-app
+const APPS = {
+  MainComponent,
+};
 
-ReactDOM.render(
-    <Main />,
-    document.getElementById('root')
-  );
+function renderAppInElement(el) {
+  let App = APPS[el.id];
+
+  if (!App)
+  {
+    return;
+  }
+
+  // get props from elements data attribute, like the post_id
+  const props = Object.assign({}, el.dataset);
+  ReactDOM.render(<App {...props} />, el);
+}
+
+document.querySelectorAll('.react-component').forEach(renderAppInElement);
