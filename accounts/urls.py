75183d05f6fa5e1eeab5635969c2611135124f32
bull0n <lucas.bulloni@protonmail.ch>
2019-03-01 09:50:18 +0100

    reorganising urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
     path('signup/', views.SignUp.as_view(), name='signup'),
 ]
--- movielist/urls.py
+++ movielist/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('movielistapp.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
 ]
