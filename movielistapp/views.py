839aa62378c2d70ba90e6d6bc0abf0192e53fae0
Charles-Lewis jaggi <charles-lewis.jaggi@he-arc.ch>
2019-03-11 11:33:52 +0100

    modif add movie in db

--- movielistapp/views.py
+++ movielistapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import generic, View
+from django.db import models
 from django.urls import reverse_lazy
-from .models import Movie, Person, Genre
+from .models import Movie, Person, Genre, Country, Type
 import requests
 import json
 
@@ -29,30 +30,45 @@ class Search(View):
             r = requests.get(api_request)
             f = r.json()
             m = add_json_db(f)
-            return HttpResponse(f["Genre"])
+            return HttpResponse(f["Writer"])
         return redirect('movie_detail', pk=m.id)
 
 
 def add_json_db(movie):
     if movie["Response"] == 'True':
-        actors = get_or_add_actor(movie["Actors"])
-        genres = get_or_add_genre(movie["Genre"])
+        actors = many_get_or_add_in_db(movie["Actors"], Person)
+        genres = many_get_or_add_in_db(movie["Genre"], Genre)
+        director = get_or_add_in_db(movie['Director'], Person)
+        countries = many_get_or_add_in_db(movie['Country'], Country)
+        type_movie = get_or_add_in_db(movie['Type'], Type)
+        writer = many_get_or_add_in_db(movie['Writer'], Person)
+        new_movie = Movie.objects.create(imdbID=movie['imdbID'], name=movie['Title'],
+                                         year=movie['Year'], released=movie['Released'],
+                                         runtime=movie['Runtime'], poster_link=movie['Poster'],
+                                         note='7.7', plot=movie['Plot'],
+                                         awards=movie['Awards'], dvd=movie['DVD'], director=director,
+                                         type=type_movie)
+        add_relation(new_movie.scenarist, writer)
+        add_relation(new_movie.actors, actors)
+        add_relation(new_movie.country, countries)
+        add_relation(new_movie.genres, genres)
         return True
     else:
         return False
 
 
-def get_or_add_actor(str_actors):
-    list_actors = []
-    actors = str(str_actors).split(", ")
+def many_get_or_add_in_db(str_data, model: models.Model):
+    list_data = []
+    actors = str(str_data).split(", ")
     for actor in actors:
-        list_actors.append(Person.objects.get_or_create(name=actor))
-    return list_actors
+        list_data.append(model.objects.get_or_create(name=actor))
+    return list_data
 
 
-def get_or_add_genre(str_genres):
-    list_genres = []
-    genres = str(str_genres).split(", ")
-    for genre in genres:
-        list_genres.append(Genre.objects.get_or_create(name=genre))
-    return list_genres
+def get_or_add_in_db(str_data, model: models.Model):
+    return model.objects.get_or_create(name=str_data)
+
+
+def add_relation(movie, datas):
+    for data in datas:
+        movie.add(data)
