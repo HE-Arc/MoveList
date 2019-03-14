b43fc30e0305cdb8f1005d0d6273dc2357e1316a
bull0n <lucas.bulloni@protonmail.ch>
2019-03-14 16:53:01 +0100

    settings

--- movielist/settings.py
+++ movielist/settings.py
@@ -71,13 +71,13 @@ TEMPLATES = [
     },
 ]
 
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
 
-STATIC_ROOT = (
-    os.path.join(BASE_DIR, 'assets/dist')
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static'),
 
 WEBPACK_LOADER = {
     'DEFAULT': {
@@ -121,10 +121,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'my_list'
 
