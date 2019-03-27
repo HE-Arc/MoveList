34043aa43c3694cd0f1ea9bb29a5cd9a0bd72805
bull0n <lucas.bulloni@protonmail.ch>
2019-03-27 11:12:20 +0100

    allow no rating and state

--- movielistapp/listmodels/listMovie.py
+++ movielistapp/listmodels/listMovie.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class ListMovie(models.Model):
     note = models.IntegerField(null=True)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    state = models.ForeignKey(State, on_delete=models.CASCADE)
+    state = models.ForeignKey(State, on_delete=models.CASCADE, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
